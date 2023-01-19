05029a67c2a8b6dc486122648294c9ca84015bda
Maria Martinkous <marticaida@gmail.com>
2023-01-18 23:10:09 -0600

    populate table with user data from the form

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
-const myLibrary = [];
-// user input variables
-const userAuthor = document.querySelector('#author');
-const userTitle = document.querySelector('#title');
-const userPages = document.querySelector('#pages');
+const myLibrary = [
+  {
+    author: 'Jane Austen',
+    title: 'Pride and Prejudice',
+    pages: 234,
+    userRead: 'read'
+  },
+  {
+    author: 'Jane Austen',
+    title: 'Sense and Sensibility',
+    pages: 254,
+    userRead: 'read'
+  },
+  {
+    author: 'Jane Austen',
+    title: 'Mansfield Park',
+    pages: 300,
+    userRead: 'read'
+  }
+];
+
 const readStatus = document.querySelectorAll('#read');
-// button submitting input and creating the new book object
-const submit = document.querySelector('.submit');
-// capture grid
-const grid = document.querySelector('.list');
-// create prototype Book object
-function Book (author, title, pages, read) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.read = read;
-}
-// add the new book entry to the library
+const form = document.querySelector('form');
+const table = document.querySelector('table');
+const data = Object.keys(myLibrary[0]);
+
+// retrive user input from the form
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // collect user input into object
+  const formData = new FormData(form);
+  console.log(formData);
+  // create new object for every user input
+  const book = Object.fromEntries(formData);
+  // add new book to the library array
+  myLibrary.push(book);
+  // clear the table
+  table.textContent = '';
+  addToMyLibrary();
+});
+
+// receive input and add the new book entry to the library
 const addToMyLibrary = () => {
+  // initiate read status
   let userRead;
   for (const status of readStatus) {
     if (status.checked) {
@@ -24,53 +49,48 @@ const addToMyLibrary = () => {
       break;
     }
   }
-  // let book = new Book(author.value, title.value, pages.value, read.value);
-  const book = new Book(
-    userAuthor.value,
-    userTitle.value,
-    userPages.value,
-    userRead
-  );
-  // add new book to the library array
-  myLibrary.push(book);
-  // display new book in the grid
-  // for (let i = 0; i < myLibrary.length; i++) {
 
-  //  Not using myLibrary array at this time
+  // make DOM table headers
+  function generateTableHead (table, data) {
+    const thead = table.createTHead();
+    const row = thead.insertRow();
+    for (const key of data) {
+      // make cells
+      const th = document.createElement('th');
+      // populate cells with object keys as headers
+      const text = document.createTextNode(key);
+      th.appendChild(text);
+      row.appendChild(th);
+    }
+  }
 
-  // create new row in the library
-  const gridRow = document.createElement('li');
-  gridRow.setAttribute('class', 'grid');
-  const infoAuthor = document.createElement('div');
-  infoAuthor.setAttribute('class', 'author');
-  infoAuthor.textContent = book.author;
-  const infoTitle = document.createElement('div');
-  infoTitle.setAttribute('class', 'title');
-  infoTitle.textContent = book.title;
-  const infoPages = document.createElement('div');
-  infoPages.setAttribute('class', 'pages');
-  infoPages.textContent = book.pages;
-  const infoRead = document.createElement('div');
-  infoRead.setAttribute('class', 'read');
-  infoRead.textContent = book.read;
-  gridRow.appendChild(infoAuthor);
-  gridRow.appendChild(infoTitle);
-  gridRow.appendChild(infoPages);
-  gridRow.appendChild(infoRead);
-  grid.appendChild(gridRow);
-  // }
+  generateTableHead(table, data);
+
+  // make DOM table
+  function generateTable (table, data) {
+    for (const element of data) {
+      const row = table.insertRow();
+      // create and populate user input cells
+      for (const key in element) {
+        const cell = row.insertCell();
+        const text = document.createTextNode(element[key]);
+        cell.appendChild(text);
+      }
+    }
+  }
+  generateTable(table, myLibrary);
+
   // wipe out the form
   document.querySelector('form').reset();
   // close the form
   modal.style.display = 'none';
 };
-// capture submit button click
-submit.addEventListener('click', addToMyLibrary);
+
 // bring up the form for the new book
-const openForm = document.querySelector('.open-form');
+const openBtn = document.querySelector('.openBtn');
 const closeForm = document.querySelector('.close');
 const modal = document.querySelector('.modal');
-openForm.addEventListener('click', function () {
+openBtn.addEventListener('click', function () {
   modal.style.display = 'block';
 });
 closeForm.addEventListener('click', function () {
