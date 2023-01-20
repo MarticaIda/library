9cf67eb389ba7c8e87aacdb2ea684505e45a7007
Maria Martinkous <marticaida@gmail.com>
2023-01-19 23:32:28 -0600

    create columns for read status and delete book

--- script.js
+++ script.js
@@ -1,104 +1,95 @@
 const myLibrary = [
-  {
-    author: 'Jane Austen',
-    title: 'Pride and Prejudice',
-    pages: 234,
-    userRead: 'read'
-  },
-  {
-    author: 'Jane Austen',
-    title: 'Sense and Sensibility',
-    pages: 254,
-    userRead: 'read'
-  },
-  {
-    author: 'Jane Austen',
-    title: 'Mansfield Park',
-    pages: 300,
-    userRead: 'read'
-  }
+  // {'Author', 'Title', 'Pages', 'Read', 'Delete?'}
 ];
 
-const readStatus = document.querySelectorAll('#read');
+const readFormInput = document.querySelectorAll('#read');
 const form = document.querySelector('form');
 const table = document.querySelector('table');
-const data = Object.keys(myLibrary[0]);
+let data;
+let readBook;
+let removeBook;
+const openBtn = document.querySelector('.openBtn');
+const closeForm = document.querySelector('.close');
+const modal = document.querySelector('.modal');
 
-// retrive user input from the form
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  // collect user input into object
-  const formData = new FormData(form);
-  console.log(formData);
-  // create new object for every user input
-  const book = Object.fromEntries(formData);
-  // add new book to the library array
-  myLibrary.push(book);
-  // clear the table
-  table.textContent = '';
+  const formData = new FormData(form); // collect user input into object
+  const book = Object.fromEntries(formData); // create new object for every user input
+  myLibrary.push(book); // add new book to the library array
+  data = Object.keys(myLibrary[0]);
+  table.textContent = ''; // clear the table
   addToMyLibrary();
-});
+}); // retrive user input from the form
 
-// receive input and add the new book entry to the library
 const addToMyLibrary = () => {
-  // initiate read status
-  let userRead;
-  for (const status of readStatus) {
+  let read; // initiate read status
+  for (const status of readFormInput) {
     if (status.checked) {
-      userRead = status.value;
+      read = status.value;
       break;
     }
   }
 
-  // make DOM table headers
   function generateTableHead (table, data) {
     const thead = table.createTHead();
+    table.border = '1';
     const row = thead.insertRow();
     for (const key of data) {
-      // make cells
-      const th = document.createElement('th');
-      // populate cells with object keys as headers
-      const text = document.createTextNode(key);
+      const th = document.createElement('th'); // make header cells
+      const text = document.createTextNode(key); // populate cells with object keys as headers
       th.appendChild(text);
       row.appendChild(th);
     }
-  }
+    const deleteHdCell = document.createElement('th'); // create a header cell for Delete column
+    row.appendChild(deleteHdCell);
+    deleteHdCell.textContent = 'Delete?';
+  } // make DOM table headers
 
-  generateTableHead(table, data);
-
-  // make DOM table
   function generateTable (table, data) {
     for (const element of data) {
       const row = table.insertRow();
-      // create and populate user input cells
       for (const key in element) {
         const cell = row.insertCell();
         const text = document.createTextNode(element[key]);
         cell.appendChild(text);
-      }
+      } // create and populate user input cells
+      const deletedCell = row.insertCell(-1);
+      const deleteBook = document.createElement('input');
+      deleteBook.setAttribute('type', 'checkbox'); // create checkbox to delete a book
+      deletedCell.appendChild(deleteBook);
     }
-  }
+  } // make DOM table
+
   generateTable(table, myLibrary);
+  generateTableHead(table, data);
 
-  // wipe out the form
-  document.querySelector('form').reset();
-  // close the form
-  modal.style.display = 'none';
-};
+  // function appendColumn () {
+  //   const row = document.querySelector('tr');
+  //   let i;
+  //   // open loop for each row and append cell
+  //   for (i = 0; i < table.rows.length; i++) {
+  //     removeBook = row.insertCell(-1);
+  //   }
+  // }
 
-// bring up the form for the new book
-const openBtn = document.querySelector('.openBtn');
-const closeForm = document.querySelector('.close');
-const modal = document.querySelector('.modal');
+  readBook = document.querySelectorAll('tr td:nth-child(4)'); // select all cells with read status
+  removeBook = document.querySelectorAll('tr td:nth-child(5)'); // select all cells with delete checkbox
+
+  document.querySelector('form').reset(); // wipe out the form
+
+  modal.style.display = 'none'; // close the form
+}; // receive input and add the new book entry to the library
+
 openBtn.addEventListener('click', function () {
   modal.style.display = 'block';
-});
+}); // bring up the form for the new book
 closeForm.addEventListener('click', function () {
   modal.style.display = 'none';
 });
-// close the form by clicking anywhere on the modal
+
 window.addEventListener('click', function (event) {
   if (event.target === modal) {
     modal.style.display = 'none';
   }
-});
+}); // close the form by clicking anywhere on the modal
