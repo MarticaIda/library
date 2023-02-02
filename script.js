3f0e7efc34d49f8ed20d8822b37f0bd42a636e22
Maria Martinkous <marticaida@gmail.com>
2023-02-01 23:03:13 -0600

    make read status menu display on every book

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const myLibrary = [];
 
+const body = document.querySelector('body');
 const form = document.querySelector('form');
 const table = document.querySelector('table');
-const body = document.querySelector('body');
-let readValue;
+const tbody = document.querySelector('tbody');
 const openBtn = document.querySelector('.openBtn');
 const closeForm = document.querySelector('.close');
 const modal = document.querySelector('.modal');
@@ -22,51 +22,64 @@ window.addEventListener('click', function (event) {
   }
 });
 
-function Book (author, title, pages, readValue) {
+function Book (author, title, pages, read) {
   this.author = author;
   this.title = title;
   this.pages = pages;
-  this.read = readValue;
+  this.read = read;
 }
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  readValue = document.querySelector('input[name="read"]:checked').value;
 
   const book = new Book(
     document.getElementById('author').value,
     document.getElementById('title').value,
     document.getElementById('pages').value,
-    readValue
+    document.querySelector('input[name="read"]:checked').value
   );
   myLibrary.push(book);
-  displayMyLibrary();
+  generateTable();
 });
 
-function displayMyLibrary () {
+const generateTable = function () {
+  let temp;
   table.style.display = 'block';
   const rowCount = table.rows.length;
   for (let x = rowCount - 1; x > 0; x--) {
     table.deleteRow(x);
   }
-  function generateTable () {
-    myLibrary.forEach((book) => {
-      const row = table.insertRow(-1);
-      const data = Object.values(book);
-      data.forEach((value) => (row.insertCell().textContent = value));
-      const deleteBook = row.insertCell(4);
-      deleteBook.innerHTML = 'Delete';
-    });
+  for (let i = 0; i < myLibrary.length; i++) {
+    const row = tbody.insertRow(-1);
+    const data = Object.values(myLibrary[i]);
+    data.forEach((value) => (row.insertCell().textContent = value));
+    const dCell = row.insertCell(-1);
+    const input = document.createElement('input');
+    input.setAttribute('type', 'checkbox'); // create checkbox to delete a book
+    dCell.appendChild(input);
+    temp = myLibrary[i].read;
+
+    const rCell = document.querySelectorAll('tr td:nth-child(4)');
+    const select = document.createElement('select');
+    select.id = 'select';
+    const options = ['yes', 'no'];
+    for (let z = 0; z < options.length; z++) {
+      const option = document.createElement('option');
+      option.value = options[z];
+      option.text = options[z];
+      select.appendChild(option);
+      if (option.value === temp) {
+        option.selected = true;
+        for (let y = 0; y < rCell.length; y++) {
+          rCell[y].appendChild(select);
+        }
+      }
+    }
   }
-
-  generateTable(table, myLibrary);
-
   form.reset();
-
   modal.style.display = 'none';
-
   body.appendChild(deleteBtn);
-}
+};
 
 closeForm.addEventListener('click', function () {
   modal.style.display = 'none';
