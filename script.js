e88fe41c41041ce65032048585480ef2a58ae14e
Maria Martinkous <marticaida@gmail.com>
2023-02-01 23:43:01 -0600

    make read property in book objects change

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const openBtn = document.querySelector('.openBtn');
 const closeForm = document.querySelector('.close');
 const modal = document.querySelector('.modal');
 const deleteBtn = document.createElement('button');
+let temp2;
 
 deleteBtn.setAttribute('class', 'btn');
 deleteBtn.textContent = 'Delete';
@@ -46,8 +47,8 @@ const generateTable = function () {
   let temp;
   table.style.display = 'block';
   const rowCount = table.rows.length;
-  for (let x = rowCount - 1; x > 0; x--) {
-    table.deleteRow(x);
+  for (let i = rowCount - 1; i > 0; i--) {
+    table.deleteRow(i);
   }
   for (let i = 0; i < myLibrary.length; i++) {
     const row = tbody.insertRow(-1);
@@ -55,7 +56,7 @@ const generateTable = function () {
     data.forEach((value) => (row.insertCell().textContent = value));
     const dCell = row.insertCell(-1);
     const input = document.createElement('input');
-    input.setAttribute('type', 'checkbox'); // create checkbox to delete a book
+    input.setAttribute('type', 'checkbox');
     dCell.appendChild(input);
     temp = myLibrary[i].read;
 
@@ -63,24 +64,28 @@ const generateTable = function () {
     const select = document.createElement('select');
     select.id = 'select';
     const options = ['yes', 'no'];
-    for (let z = 0; z < options.length; z++) {
+    for (let i = 0; i < options.length; i++) {
       const option = document.createElement('option');
-      option.value = options[z];
-      option.text = options[z];
+      option.value = options[i];
+      option.text = options[i];
       select.appendChild(option);
       if (option.value === temp) {
         option.selected = true;
-        for (let y = 0; y < rCell.length; y++) {
-          rCell[y].appendChild(select);
+        temp2 = option.value;
+        for (let i = 0; i < rCell.length; i++) {
+          rCell[i].appendChild(select);
         }
       }
     }
+    select.addEventListener('change', (e) => {
+      myLibrary[i].read = select.value;
+    });
   }
+  console.log(temp2);
   form.reset();
   modal.style.display = 'none';
   body.appendChild(deleteBtn);
 };
-
 closeForm.addEventListener('click', function () {
   modal.style.display = 'none';
 });
